4edb654062e3dc3f2cefcf891dfb91ef4a38a284
Fakerick1 <rickdotyt@gmail.com>
2017-11-02 12:34:38 +0100

    Bugfixes 1

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/MainApp.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
