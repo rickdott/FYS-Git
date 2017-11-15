5745b29fecbcd5ade931a92dcb041de34da7fe66
appelmoesje <stanvanweringh@gmail.com>
2017-11-15 10:50:46 +0100

    Class gemaakt
    Class gemaakt voor Excel (zonder functies)

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/ReadExcel.java
@@ -0,0 +1,15 @@
+package com.mycompany.mavenproject2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class ReadExcel {
+    // TODO: Hier moet de logica komen voor excel inlezen
+}
