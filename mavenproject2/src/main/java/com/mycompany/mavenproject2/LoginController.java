d84642e4824aa63d2bf9f08a6d6f7e96a2e95c05
appelmoesje <stanvanweringh@gmail.com>
2017-11-28 14:42:03 +0100

    LoginController languages methods

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/LoginController.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/LoginController.java
@@ -8,6 +8,7 @@ package com.mycompany.mavenproject2;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,19 +17,61 @@ import javafx.scene.layout.AnchorPane;
 
 /**
  *
- * @author Rick
+ * @author Staick
  */
-public class LoginController implements Initializable{
+public class LoginController implements Initializable {
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    
+
     @FXML
     private AnchorPane paneLogin;
-    
+    private ResourceBundle bundle;
+    private Locale locale;
+
     FXMLDocumentController controller = new FXMLDocumentController();
-    
+
+    // Methods for changing the language
+    @FXML
+    private void setLanguageEnglish() {
+        System.out.println("Set language to English");
+        loadLanguage("en", "EN");
+    }
+
+    @FXML
+    private void setLanguageDutch() {
+        System.out.println("Set language to Dutch");
+        loadLanguage("nl", "NL");
+    }
+
+    @FXML
+    private void setLanguageGerman() {
+        System.out.println("Set language to German");
+        //loadLanguage("");
+    }
+
+    @FXML
+    private void setLanguagePortuguese() {
+        System.out.println("Set language to Portuguese");
+        //loadLanguage("");
+    }
+
+    @FXML
+    private void setLanguageTurkish() {
+        System.out.println("Set language to Turkish");
+        //loadLanguage("");
+    }
+
+    // Main method for changing languages WERKT NIET (PATH IN .getBundle() kan niet worden gevonden)
+    private void loadLanguage(String language, String lang) {
+
+        Locale.setDefault(new Locale(language, lang));
+        bundle = ResourceBundle.getBundle("lang");
+        System.out.println("GVD WERK EENS! : " + bundle.getString("language"));
+    }
+
     // Method for creating a PDF ---MOVE TO RELEVANT CONTROLLER
     @FXML
     private void createPdf() {
@@ -37,7 +80,7 @@ public class LoginController implements Initializable{
         pdf.printPDF();
         System.out.println("PDF Created...");
     }
-    
+
     // Method for sending an e-mail ---MOVE TO RELEVANT CONTROLLER
     @FXML
     private void sendMail() {
@@ -46,7 +89,7 @@ public class LoginController implements Initializable{
         mail.mailsturen();
         System.out.println("Mail sent...");
     }
-    
+
     // Method om een Excelsheet te importeren ---MOVE TO RELEVANT CONTROLLER
     @FXML
     private void excelImport() {
@@ -62,7 +105,7 @@ public class LoginController implements Initializable{
         row = reader.getNextRow(); // Eerste row
         Database db = new Database();
         while (row != null) {
-            
+
             String mail = row.get(row.size() - 1);
             System.out.println(mail);
 
@@ -73,14 +116,14 @@ public class LoginController implements Initializable{
             // SQL query die alles invoert in de database
             sql = String.format("INSERT INTO `Bagage`(`labelnumber`, `flightnumber`, `destination`, `type`, `brand`, `colour`, `specialchar`, `passengerid`, `foundat`, `foundatdate`, `date`)VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')", row.get(0), row.get(1), row.get(2), row.get(3), row.get(4), row.get(5), row.get(6), idpassenger, row.get(7), row.get(8), row.get(9));
             db.executeUpdateQuery(sql);
-            
+
             // Pakt alvast de volgende row
             row = reader.getNextRow();
         }
 
         System.out.println("Excel import complete...");
     }
-    
+
     @FXML
     private void openCustomerHomescreen(ActionEvent event) {
         controller.newAnchorpane("CustomerHomescreen", paneLogin);
@@ -90,5 +133,5 @@ public class LoginController implements Initializable{
     private void openWorkerHomescreen(ActionEvent event) {
         controller.newAnchorpane("WorkerHomescreen", paneLogin);
     }
-    
+
 }
