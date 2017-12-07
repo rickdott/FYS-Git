d76b64cce1b1ddeaaa7728a39896d9dac714448d
fakerick1 <rickdotyt@gmail.com>
2017-12-07 11:32:37 +0100

    Added making a choice

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/RequestStatusController.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/RequestStatusController.java
@@ -21,6 +21,8 @@ import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -38,11 +40,14 @@ public class RequestStatusController implements Initializable {
             = FXCollections.observableArrayList();
 
     @FXML
+    private ToggleGroup lostFoundGroup;
+
+    @FXML
     private AnchorPane requestpage;
 
     @FXML
     private VBox vbox1, vbox2;
-    
+
     @FXML
     private TextField regNrField, dateFoundField, timeFoundField, typeField,
             brandField, flightNrField, labelNrField, locationFoundField,
@@ -73,7 +78,7 @@ public class RequestStatusController implements Initializable {
     @FXML
     private void getInput() throws SQLException {
         String query = getQueryFromTextfields();
-                
+
         // Create database connection, execute the query
         Database database = new Database();
         ResultSet result = database.executeResultSetQuery(query);
@@ -83,80 +88,144 @@ public class RequestStatusController implements Initializable {
         // for every result, adding the attributes of the result to the 
         // corresponding attribute in the FoundLuggage object
         foundLuggageList = Utilities.initializeLuggageFromResultSet(result, foundLuggageList);
-
+        
         result.close();
         database.close();
-        
+
         // Show the TableView
         makeTextFieldsAndLabelsInvisible();
         foundLuggageTableView.setVisible(true);
     }
-
+    
     @FXML
     private String getQueryFromTextfields() {
         // Creates new List of Strings to be included in the query
         List<String> queryList = new ArrayList<String>();
-
-        if (!regNrField.getText().isEmpty()) {
-            queryList.add("registrationnr = '" + regNrField.getText() + "' ");
-        }
-
-        if (!dateFoundField.getText().isEmpty()) {
-            queryList.add("datefound = '" + dateFoundField.getText() + "' ");
-        }
-
-        if (!timeFoundField.getText().isEmpty()) {
-            queryList.add("timefound = '" + timeFoundField.getText() + "' ");
-        }
-
-        if (!typeField.getText().isEmpty()) {
-            queryList.add("luggagetype = '" + typeField.getText() + "' ");
-        }
-
-        if (!brandField.getText().isEmpty()) {
-            queryList.add("brand = '" + brandField.getText() + "' ");
-        }
-
-        if (!flightNrField.getText().isEmpty()) {
-            queryList.add("flightnumber = '" + flightNrField.getText() + "' ");
-        }
-
-        if (!labelNrField.getText().isEmpty()) {
-            queryList.add("luggagelabelnr = '" + labelNrField.getText() + "' ");
-        }
-
-        if (!locationFoundField.getText().isEmpty()) {
-            queryList.add("locationfound = '" + locationFoundField.getText() + "' ");
-        }
-
-        if (!mainColourField.getText().isEmpty()) {
-            queryList.add("primarycolour = '" + mainColourField.getText() + "' ");
-        }
-
-        if (!secondaryColourField.getText().isEmpty()) {
-            queryList.add("secondarycolour = '" + secondaryColourField.getText() + "' ");
-        }
-
-        if (!sizeField.getText().isEmpty()) {
-            queryList.add("size = '" + sizeField.getText() + "' ");
-        }
-
-        if (!weightField.getText().isEmpty()) {
-            queryList.add("weight = '" + weightField.getText() + "' ");
-        }
-
-        if (!nameAndCityField.getText().isEmpty()) {
-            queryList.add("passenger_name_city = '" + nameAndCityField.getText() + "' ");
-        }
-
-        if (!specialCharField.getText().isEmpty()) {
-            queryList.add("otherchar = '" + specialCharField.getText() + "' ");
-        }
+        String query;
         
-        // Start of the query
-        // FOUNDBAGAGEINVENTORY IS INVALID, FIX THIS
-        String query = "SELECT * FROM Foundbagageinventory ";
+        List<Toggle> toggleList = new ArrayList();
+        toggleList.addAll(lostFoundGroup.getToggles());
+        
+        if (toggleList.get(0).isSelected()) {
+            if (!regNrField.getText().isEmpty()) {
+                queryList.add("registrationnr = '" + regNrField.getText() + "' ");
+            }
 
+            if (!dateFoundField.getText().isEmpty()) {
+                queryList.add("datefound = '" + dateFoundField.getText() + "' ");
+            }
+
+            if (!timeFoundField.getText().isEmpty()) {
+                queryList.add("timefound = '" + timeFoundField.getText() + "' ");
+            }
+
+            if (!typeField.getText().isEmpty()) {
+                queryList.add("luggagetype = '" + typeField.getText() + "' ");
+            }
+
+            if (!brandField.getText().isEmpty()) {
+                queryList.add("brand = '" + brandField.getText() + "' ");
+            }
+
+            if (!flightNrField.getText().isEmpty()) {
+                queryList.add("flightnumber = '" + flightNrField.getText() + "' ");
+            }
+
+            if (!labelNrField.getText().isEmpty()) {
+                queryList.add("luggagelabelnr = '" + labelNrField.getText() + "' ");
+            }
+
+            if (!locationFoundField.getText().isEmpty()) {
+                queryList.add("locationfound = '" + locationFoundField.getText() + "' ");
+            }
+
+            if (!mainColourField.getText().isEmpty()) {
+                queryList.add("primarycolour = '" + mainColourField.getText() + "' ");
+            }
+
+            if (!secondaryColourField.getText().isEmpty()) {
+                queryList.add("secondarycolour = '" + secondaryColourField.getText() + "' ");
+            }
+
+            if (!sizeField.getText().isEmpty()) {
+                queryList.add("size = '" + sizeField.getText() + "' ");
+            }
+
+            if (!weightField.getText().isEmpty()) {
+                queryList.add("weight = '" + weightField.getText() + "' ");
+            }
+
+            if (!nameAndCityField.getText().isEmpty()) {
+                queryList.add("passenger_name_city = '" + nameAndCityField.getText() + "' ");
+            }
+
+            if (!specialCharField.getText().isEmpty()) {
+                queryList.add("otherchar = '" + specialCharField.getText() + "' ");
+            }
+
+            // Start of the query
+            // FOUNDBAGAGEINVENTORY IS INVALID, FIX THIS
+            query = "SELECT * FROM Lostbagage ";
+        } else {
+            if (!regNrField.getText().isEmpty()) {
+                queryList.add("registrationnr = '" + regNrField.getText() + "' ");
+            }
+
+            if (!dateFoundField.getText().isEmpty()) {
+                queryList.add("datefound = '" + dateFoundField.getText() + "' ");
+            }
+
+            if (!timeFoundField.getText().isEmpty()) {
+                queryList.add("timefound = '" + timeFoundField.getText() + "' ");
+            }
+
+            if (!typeField.getText().isEmpty()) {
+                queryList.add("luggagetype = '" + typeField.getText() + "' ");
+            }
+
+            if (!brandField.getText().isEmpty()) {
+                queryList.add("brand = '" + brandField.getText() + "' ");
+            }
+
+            if (!flightNrField.getText().isEmpty()) {
+                queryList.add("flightnumber = '" + flightNrField.getText() + "' ");
+            }
+
+            if (!labelNrField.getText().isEmpty()) {
+                queryList.add("luggagelabelnr = '" + labelNrField.getText() + "' ");
+            }
+
+            if (!locationFoundField.getText().isEmpty()) {
+                queryList.add("locationfound = '" + locationFoundField.getText() + "' ");
+            }
+
+            if (!mainColourField.getText().isEmpty()) {
+                queryList.add("primarycolour = '" + mainColourField.getText() + "' ");
+            }
+
+            if (!secondaryColourField.getText().isEmpty()) {
+                queryList.add("secondarycolour = '" + secondaryColourField.getText() + "' ");
+            }
+
+            if (!sizeField.getText().isEmpty()) {
+                queryList.add("size = '" + sizeField.getText() + "' ");
+            }
+
+            if (!weightField.getText().isEmpty()) {
+                queryList.add("weight = '" + weightField.getText() + "' ");
+            }
+
+            if (!nameAndCityField.getText().isEmpty()) {
+                queryList.add("passenger_name_city = '" + nameAndCityField.getText() + "' ");
+            }
+
+            if (!specialCharField.getText().isEmpty()) {
+                queryList.add("otherchar = '" + specialCharField.getText() + "' ");
+            }
+
+            // Start of the query
+            query = "SELECT * FROM Foundbagageinventory ";
+        }
         // Loop through the list of Strings to add to the query, adding 
         // 'WHERE ' in front if it's the first, adding 'AND ' in front if it's 
         // not, to create a valid SQL Query
@@ -171,7 +240,7 @@ public class RequestStatusController implements Initializable {
         }
         return query;
     }
-    
+
     public void makeTextFieldsAndLabelsInvisible() {
         // Create an ObservableList consisting of all children nodes of the 
         // vboxes, then making them invisible
@@ -179,13 +248,13 @@ public class RequestStatusController implements Initializable {
         for (Node node : vbox1Children) {
             node.setVisible(false);
         }
-        
+
         ObservableList<Node> vbox2Children = vbox2.getChildren();
         for (Node node : vbox2Children) {
             node.setVisible(false);
         }
     }
-    
+
     public void makeTextFieldsAndLabelsVisible() {
         // Create an ObservableList consisting of all children nodes of the 
         // vboxes, then making them visible
@@ -193,7 +262,7 @@ public class RequestStatusController implements Initializable {
         for (Node node : vbox1Children) {
             node.setVisible(true);
         }
-        
+
         ObservableList<Node> vbox2Children = vbox2.getChildren();
         for (Node node : vbox2Children) {
             node.setVisible(true);
