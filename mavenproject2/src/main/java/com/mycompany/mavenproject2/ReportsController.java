a760686fbefb719a1a87dfc08668352f903b404f
Fakerick1 <rickdotyt@gmail.com>
2017-12-22 12:57:05 +0100

    Added more reports functionality

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/ReportsController.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/ReportsController.java
@@ -6,18 +6,23 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
+import static javafx.collections.FXCollections.observableArrayList;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
@@ -30,27 +35,38 @@ public class ReportsController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-//        try {
-//            showMissingStats();
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ReportsController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+            showMissingStats();
+            ArrayList<String> listOfYears = getYearsInDB();
+            years.addAll(listOfYears);
+            yearComboBox.setItems(years);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReportsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
+    private static ObservableList<String> years = FXCollections.observableArrayList();
 
     @FXML
     private PieChart pieChart;
 
     @FXML
-    private DatePicker datePickerStart, datePickerEnd;
+    private LineChart lineChart;
 
     @FXML
     private ToggleGroup selectionGroup;
 
     @FXML
     private Toggle missingLuggageToggle, foundLuggageToggle, missingLuggagePerMonthToggle, solvedToggle, compensationToggle;
-    
+
     @FXML
-    private VBox checkBoxVBox;
+    private VBox missingVBox, missingPerMonthVBox;
+
+    @FXML
+    private ComboBox yearComboBox;
+
+    @FXML
+    private Button submitFoundMissingBut, submitMissingPerMonthBut;
 
     class ChartEntry {
 
@@ -70,27 +86,51 @@ public class ReportsController implements Initializable {
             return entry;
         }
 
+        public void incrementAmount(double amount) {
+            this.amount += amount;
+        }
+
     }
 
     @FXML
     private void showMissingStats() throws SQLException {
         System.out.println("Showing missing stats");
+        lineChart.setVisible(false);
         pieChart.setVisible(true);
-        
-        populatePieChart(pieChart, amountOfMissingAsPieChart("Flight.from", findSelectedAirports()));
+        yearComboBox.setVisible(false);
+        submitFoundMissingBut.setVisible(true);
+        submitMissingPerMonthBut.setVisible(false);
+        populatePieChart(pieChart, amountOfMissingAsPieChart("Flight.from", "Lostbagage", findSelectedAirports()));
     }
 
     @FXML
-    private void showFoundStats() {
-        ArrayList<String> testList = findSelectedAirports();
-        for (int i = 0; i < testList.size(); i++) {
-            System.out.println(testList.get(i));
+    private void showFoundStats() throws SQLException {
+        System.out.println("Showing found stats");
+        lineChart.setVisible(false);
+        pieChart.setVisible(true);
+        yearComboBox.setVisible(false);
+        submitFoundMissingBut.setVisible(true);
+        submitMissingPerMonthBut.setVisible(false);
+        populatePieChart(pieChart, amountOfMissingAsPieChart("Flight.from", "Foundbagageinventory", findSelectedAirports()));
+    }
+
+    @FXML
+    private void showMissingPerMonthStats() throws SQLException {
+        System.out.println("Showing missing per month stats");
+        yearComboBox.setVisible(true);
+        submitFoundMissingBut.setVisible(false);
+        submitMissingPerMonthBut.setVisible(true);
+        String year;
+        if (yearComboBox.getValue() != null) {
+            year = (String) yearComboBox.getValue();
+        } else {
+            year = years.get(years.size() - 1);
         }
-    }
 
-    @FXML
-    private void showMissingPerMonthStats() {
-        System.out.println("bar");
+        populateLineChart(lineChart, findSelectedAirports(), year);
+        pieChart.setVisible(false);
+        missingPerMonthVBox.setVisible(true);
+        lineChart.setVisible(true);
     }
 
     @FXML
@@ -102,67 +142,175 @@ public class ReportsController implements Initializable {
     private void showCompensationStats() {
         System.out.println("kut");
     }
-    
-    private ArrayList<String> findSelectedAirports(){
+
+    private ArrayList<String> findSelectedAirports() {
         ArrayList<String> selectedAirports = new ArrayList<>();
-        ObservableList<Node> checkBoxList = checkBoxVBox.getChildren();
-        
+        ObservableList<Node> checkBoxList = missingVBox.getChildren();
+
         CheckBox cb;
         for (int i = 0; i < checkBoxList.size(); i++) {
-            cb = (CheckBox) checkBoxList.get(i);
-            if (cb.isSelected()) {
-                selectedAirports.add(cb.getText());
+            if (checkBoxList.get(i) instanceof CheckBox) {
+                cb = (CheckBox) checkBoxList.get(i);
+                if (cb.isSelected()) {
+                    selectedAirports.add(cb.getText());
+                }
             }
+        }
+        for (int i = 0; i < selectedAirports.size(); i++) {
+            System.out.println(selectedAirports.get(i));
         }
         return selectedAirports;
     }
 
-    @FXML
-    private String datePickerToSQLString() {
-        String date;
-        if (datePickerStart.getValue() != null) {
-            LocalDate datum = datePickerStart.getValue();
-            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-            date = datum.format(format);
-            System.out.println(datum);
-            System.out.println(datum.toString());
-            System.out.println(date);
-        } else {
-            return null;
-        }
-        return date;
-    }
-
     private void populatePieChart(PieChart chart, ArrayList<ChartEntry> entryList) throws SQLException {
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
         for (int i = 0; i < entryList.size(); i++) {
-            pieChartData.add(new PieChart.Data(entryList.get(i).getEntry(), entryList.get(i).getAmount()));
+            pieChartData.add(new PieChart.Data(entryList.get(i).getEntry() + ", " + (int) entryList.get(i).getAmount(), entryList.get(i).getAmount()));
         }
-        
+
         chart.setData(pieChartData);
-        chart.setTitle("Missing Luggage");
     }
 
-    private ArrayList amountOfMissingAsPieChart(String category, ArrayList<String> airports) {
+    private ArrayList amountOfMissingAsPieChart(String category, String lostOrFound, ArrayList<String> airports) throws SQLException {
         ArrayList<ChartEntry> entryList = new ArrayList<>();
-        try {
-            String query = String.format("SELECT COUNT(registrationnr) AS amount, %s\n" +
-                                         "FROM Foundbagageinventory INNER JOIN Flight ON flightnr = flightnumber\n" +
-                                         "GROUP BY flightnumber", category);
 
-            Database database = new Database();
-            ResultSet result = database.executeResultSetQuery(query);
-            while (result.next()) {
-                ChartEntry entry = new ChartEntry(result.getInt("amount"), result.getString("from"));
-                entryList.add(entry);
+        String query = String.format("SELECT COUNT(registrationnr) AS amount, %s\n"
+                + "FROM %s INNER JOIN Flight ON flightnr = flightnumber\n",
+                category, lostOrFound);
+        if (!airports.isEmpty()) {
+            for (int i = 0; i < airports.size(); i++) {
+                if (i == 0) {
+                    query += "WHERE ";
+                } else if (i == airports.size() - 1) {
+                    query += String.format("%s = '%s' ", category, airports.get(i));
+                } else {
+                    query += String.format("%s = '%s' OR ", category, airports.get(i));
+                }
             }
-
-            result.close();
-            database.close();
-
-        } catch (SQLException ex) {
-            Logger.getLogger(ReportsController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        query += "GROUP BY flightnumber";
+
+        Database database = new Database();
+        ResultSet result = database.executeResultSetQuery(query);
+        while (result.next()) {
+            ChartEntry entry = new ChartEntry(result.getInt("amount"), result.getString("from"));
+            entryList.add(entry);
+        }
+
+        for (int i = 0; i < entryList.size(); i++) {
+            String value = entryList.get(i).getEntry();
+
+            for (int j = i + 1; j < entryList.size(); j++) {
+                if (value.equals(entryList.get(j).getEntry())) {
+                    entryList.get(i).incrementAmount(entryList.get(j).getAmount());
+                    entryList.remove(j);
+                    j--;
+                }
+            }
+        }
+
+        result.close();
+        database.close();
+
         return entryList;
     }
+
+    private void populateLineChart(LineChart chart, ArrayList<String> airports, String year) throws SQLException {
+        chart.setAnimated(false);
+        chart.getData().clear();
+
+        String query = "SELECT datefound\n"
+                + "FROM Foundbagageinventory INNER JOIN Flight ON flightnr = flightnumber\n"
+                + "WHERE datefound LIKE '%" + year + "' ";
+
+        if (!airports.isEmpty()) {
+            for (int i = 0; i < airports.size(); i++) {
+                if (i == 0) {
+                    query += "AND Flight.from IN(";
+                } else if (i == airports.size() - 1) {
+                    query += String.format("'%s') ", airports.get(i));
+                } else {
+                    query += String.format("'%s',  ", airports.get(i));
+                }
+            }
+        }
+
+        Database database = new Database();
+        ResultSet result = database.executeResultSetQuery(query);
+
+        int[] amountArray = new int[12];
+        String[] monthArray = {"jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"};
+        while (result.next()) {
+            String currentMonth = result.getString("datefound").substring(3, 6);
+            switch (currentMonth) {
+                case "jan":
+                    amountArray[0]++;
+                    break;
+                case "feb":
+                    amountArray[1]++;
+                    break;
+                case "mrt":
+                    amountArray[2]++;
+                    break;
+                case "apr":
+                    amountArray[3]++;
+                    break;
+                case "mei":
+                    amountArray[4]++;
+                    break;
+                case "jun":
+                    amountArray[5]++;
+                    break;
+                case "jul":
+                    amountArray[6]++;
+                    break;
+                case "aug":
+                    amountArray[7]++;
+                    break;
+                case "sep":
+                    amountArray[8]++;
+                    break;
+                case "okt":
+                    amountArray[9]++;
+                    break;
+                case "nov":
+                    amountArray[10]++;
+                    break;
+                case "dec":
+                    amountArray[11]++;
+                    break;
+                default:
+                    System.out.println("default called");
+            }
+        }
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Month");
+
+        XYChart.Series data = new XYChart.Series();
+        data.setName("Antalya");
+        for (int i = 0; i < monthArray.length; i++) {
+            data.getData().add(new XYChart.Data(monthArray[i], amountArray[i]));
+        }
+        chart.getData().add(data);
+
+    }
+
+    private ArrayList<String> getYearsInDB() throws SQLException {
+        Database database = new Database();
+        String query = "SELECT datefound FROM stanviw199_fys.Foundbagageinventory";
+        ArrayList<String> list = new ArrayList<>();
+        ResultSet result = database.executeResultSetQuery(query);
+        while (result.next()) {
+            String year = result.getString("datefound").substring(7, 11);
+            if (!list.contains(year)) {
+                list.add(year);
+            }
+        }
+
+        return list;
+    }
+
 }
